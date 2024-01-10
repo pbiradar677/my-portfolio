597670cbeec425a3988cd195e8c37f94ecef911c
Parshuram <parshuram@blitzcodesys.com>
2024-01-10 15:40:43 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>My Portfolio</div>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx,tsx,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
