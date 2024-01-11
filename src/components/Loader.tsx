1e3ff3624dada2ebfadeb599ae28c02f866198ad
Parshuram <parshuram@blitzcodesys.com>
2024-01-12 00:47:07 +0530

    hero and computer section

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,22 @@
- 
-const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
-}
+import { Html, useProgress } from "@react-three/drei";
 
-export default Loader
+const Loader = () => {
+  const { progress } = useProgress();
+  return (
+    <Html>
+      <span className="canvas-load"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#f1f1f1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}
+      </p>
+    </Html>
+  );
+};
+
+export default Loader;
